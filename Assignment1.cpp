0ac0aa97b93d2abd7cf92100348afdf4733320c3
jgaudana <73472322+jgaudana@users.noreply.github.com>
2023-05-13 15:13:46 +0530

    Add files via upload

---
+++ Assignment1.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+#include <string>
+
+class car
+{
+public :
+	int Release_year;
+	int car_speed;
+	string marker_of_car;
+
+	void get_value()
+	{
+	   cout << "enter relase yearof car:\n ";
+       cin >> Release_year;
+       cout << "enter car speed:\n "; 
+       cin >> car_speed;
+       cout << "enter marker of car:\n "; 
+       cin >> marker_of_car; 
+	}
+	 
+	 void acclerate()
+	 { 
+	  car_speed=car_speed+10;
+	 }
+	 void brake()
+	 {
+	  car_speed=car_speed-10;
+	 }
+};
+
+
+
+
+main()
+{
+car martin;
+martin.get_value();
+int i=0;
+for (i=0;i<5;i++)
+{
+martin.acclerate();
+}
+cout << martin.car_speed;
+cout <<"\n";
+for (i=0;i<5;i++)
+{
+martin.brake();
+}
+cout << martin.car_speed;
+
+}
